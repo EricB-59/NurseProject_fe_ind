dee071036ffa78f8594f0980182e7437add2a1fd
EricB-59 <ebaenac59@gmail.com>
2024-11-25 17:10:32 +0100

    eric

--- src/app/search-by-name/search-by-name.component.ts
+++ src/app/search-by-name/search-by-name.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-by-name',
   templateUrl: './search-by-name.component.html',
-  styleUrl: './search-by-name.component.css'
+  styleUrl: './search-by-name.component.css',
 })
 export class SearchByNameComponent {
+  data = {
+    name: 'John Doe',
+    age: 30,
+    city: 'New York',
+  };
 
+  name = this.data.name;
+  age = this.data.age;
 }
