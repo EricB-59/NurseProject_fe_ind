c609cfe6fe8b28a1d882086d71c665a4e8bf6535
EricB-59 <ebaenac59@gmail.com>
2024-11-25 16:26:20 +0100

    first_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchByNameComponent } from './search-by-name/search-by-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchByNameComponent
   ],
   imports: [
     BrowserModule,
