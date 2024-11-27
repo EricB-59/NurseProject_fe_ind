66cd678185490b5e6d84aeb88748b7534eeb51b9
EricB-59 <ebaenac59@gmail.com>
2024-11-27 09:14:59 +0100

    search name done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchByNameComponent } from './search-by-name/search-by-name.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, SearchByNameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/search-by-name/search-by-name.component.ts
+++ src/app/search-by-name/search-by-name.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import jsonData from '../data/DATA.json';
 
 @Component({
@@ -7,5 +8,29 @@ import jsonData from '../data/DATA.json';
   styleUrl: './search-by-name.component.css',
 })
 export class SearchByNameComponent {
+  // User data from src/app/data/DATA.json
   users: any[] = jsonData;
+
+  searchName = new FormGroup({
+    name: new FormControl(''),
+  });
+
+  searched: boolean = false;
+  nameSearched: string = '';
+
+  submitSearch() {
+    this.searched = false;
+    this.nameSearched = '';
+
+    this.users.forEach((value) => {
+      if (this.searchName.value.name === value.first_name) {
+        this.searched = true;
+        this.nameSearched = value.first_name;
+      }
+    });
+
+    if (!this.searched) {
+      console.log('No se encontró el nombre');
+    }
+  }
 }
